13b05bd85719f6715007bb91e505ddb31de826fd
SohamAgawane <sohamagavane@gmail.com>
2026-02-13 19:59:10 +0530

    acess control, in-built packages & object classes method

---
+++ src/com/soham/accesscontrol10/A.java
@@ -0,0 +1,30 @@
+package com.soham.accesscontrol10;
+
+public class A {
+    private int num;
+    protected String name;
+    int[] arr;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public A(int num, String name) {
+        this.num = num;
+        this.name = name;
+        this.arr = new int[num];
+    }
+
+}
---
+++ src/com/soham/accesscontrol10/Main.java
@@ -0,0 +1,13 @@
+package com.soham.accesscontrol10;
+
+public class Main {
+    public static void main(String[] args) {
+        A obj = new A(7, "Soham");
+
+        obj.getNum();
+        obj.setNum(7);
+
+        obj.getName();
+        obj.setName("Soham");
+    }
+}
